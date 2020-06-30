60415b87d689ca4e289106d13ed0072810e9b6be
EricLin <eric888a@hotmail.com.tw>
2020-06-30 08:10:52 +0800

    ADD model using

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using _Netcore_practice.Models;
 
 namespace _Netcore_practice
 {
